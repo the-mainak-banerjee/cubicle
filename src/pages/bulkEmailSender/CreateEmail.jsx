747da272cad1a756730fe18b6e3daa63541c0506
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-08-26 16:01:45 +0530

    added email preview popup in email edit and sent page

--- src/pages/bulkEmailSender/CreateEmail.jsx
+++ src/pages/bulkEmailSender/CreateEmail.jsx
@@ -3,13 +3,16 @@ import Skeleton from '../../components/ui/skeleton/Skeleton'
 import EmailSenderFooter from '../../components/bulkEmailSender/EmailSenderFooter'
 import { useNavigate, useParams } from 'react-router-dom'
 import EmailSenderHeaderAction from '../../components/bulkEmailSender/EmailSenderHeaderAction'
-import { Box } from '@chakra-ui/react'
+import { Box, useDisclosure } from '@chakra-ui/react'
 import EmailEditor from '../../components/bulkEmailSender/EmailEditor'
+import EmailPreviewPopup from '../../components/bulkEmailSender/EmailPreviewPopup'
 
 const CreateEmail = () => {
 
   const params = useParams()
   const navigate = useNavigate()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
 
   const handleContinueButtonAction = () => {
     navigate(`/emails/${params.emailId}/send`)
@@ -22,14 +25,20 @@ const CreateEmail = () => {
         title='Email Name'
         rightContent={
           <>
-            <EmailSenderHeaderAction/>
+            <EmailSenderHeaderAction
+              onOpen={onOpen}
+            />
           </>
         }
       >
         <Box pb='32'>
           <EmailEditor/>
         </Box>
-
+        
+       <EmailPreviewPopup
+        isOpen={isOpen} 
+        onClose={onClose}
+       />
       </Skeleton>
         <EmailSenderFooter
           buttonAction={handleContinueButtonAction}
