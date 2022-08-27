feaeb2cbe75fc43f287ffea241d61a35acfdb4c1
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-08-27 14:28:06 +0530

    created redux store

--- src/pages/databse/Database.jsx
+++ src/pages/databse/Database.jsx
@@ -1,6 +1,4 @@
-import { Text } from '@chakra-ui/react'
 import React from 'react'
-import Demo from '../../components/database/Demo'
 import Skeleton from '../../components/ui/skeleton/Skeleton'
 const Database = () => {
   return (
