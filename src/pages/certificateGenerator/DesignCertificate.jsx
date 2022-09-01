c816b16e43905cf0ce6a50c5242c3fc453648e8b
Pankaj Desai <desaipankaj740@gmail.com>
2022-09-01 16:46:06 +0530

    templates

--- src/pages/certificateGenerator/DesignCertificate.jsx
+++ src/pages/certificateGenerator/DesignCertificate.jsx
@@ -1,4 +1,4 @@
-import { Text , Box , Wrap , WrapItem } from '@chakra-ui/react';
+import { Text, Box, Wrap, WrapItem } from '@chakra-ui/react';
 import React from 'react';
 import Skeleton from '../../components/ui/skeleton/Skeleton'
 
@@ -8,30 +8,31 @@ const DesignCertificate = () => {
       <Skeleton
         isBack={true}
         title='Create Certificate'
-        isSearch={false}
+        isSearch={true}
+        buttonText='Create Design'
       >
 
-    
-        
-        <Wrap  spacing="70px" m="auto" justify="center" justifycontents="center" >
-        <WrapItem  >
-          <Box >
-        <img src="https://i.pinimg.com/originals/54/04/c9/5404c9e991a6b8e56db353c4d223f07e.jpg" alt="template" height="200px" width="300px"/>
-        <Text mt={5} ml={5} >My Design #1</Text>
-        </Box>
-        </WrapItem>
-        
-        <WrapItem>
-        
-        <Box>
-        <img src="https://i.pinimg.com/originals/54/04/c9/5404c9e991a6b8e56db353c4d223f07e.jpg" alt="tamplate" height="200px" width="300px"/>
-        <Text mt={5} ml={5}>My Design #2</Text> 
-        </Box>  
-        </WrapItem> 
-        
-          
+
+
+        <Wrap spacing="70px" m="auto" justify="center" justifycontents="center" >
+          <WrapItem  >
+            <Box bg="#eeeee4">
+              <img src="https://i.pinimg.com/originals/54/04/c9/5404c9e991a6b8e56db353c4d223f07e.jpg" alt="template" height="200px" width="300px" />
+              <Text mt={4} mb={5} ml={4} >My Design #1</Text>
+            </Box>
+          </WrapItem>
+
+          <WrapItem>
+
+            <Box bg="#eeeee4">
+              <img src="https://i.pinimg.com/originals/54/04/c9/5404c9e991a6b8e56db353c4d223f07e.jpg" alt="tamplate" height="200px" width="300px" />
+              <Text mt={4} mb={5} ml={4}>My Design #2</Text>
+            </Box>
+          </WrapItem>
+
+
         </Wrap>
-       
+
 
       </Skeleton>
     </>
